d556b3fbb1cdd3c2757ad6f4f338aa00c3041338
asierfdln <asierfdln@gmail.com>
2022-12-06 11:24:25 +0100

    Zerodetect signal in ALU as or_reduce

--- mystuff/src/alu.vhd
+++ mystuff/src/alu.vhd
@@ -23,6 +23,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_MISC.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -41,6 +42,7 @@ entity alu is
         i_a         : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
         i_b         : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
         i_alucont   : in  STD_LOGIC_VECTOR(2 downto 0);
+        o_zerodet   : out STD_LOGIC;
         o_result    : out STD_LOGIC_VECTOR(g_width-1 downto 0)
     );
 end alu;
@@ -67,22 +69,28 @@ architecture Behavioral of alu is
     signal s_b2   : STD_LOGIC_VECTOR(g_width-1 downto 0);
     signal s_sum  : STD_LOGIC_VECTOR(g_width-1 downto 0);
     signal s_slt  : STD_LOGIC_VECTOR(g_width-1 downto 0);
+    signal preout : STD_LOGIC_VECTOR(g_width-1 downto 0);
 
 begin
 
     -- for sub operation "i_a - i_b", see next line
     s_b2 <= not i_b when i_alucont(2) = '1' else
             i_b;
+
     -- s_sum signal stores value of both s_sum and sub
     s_sum <= i_a + s_b2 + i_alucont(2);
+
     -- s_slt should be 1 if most significant bit of s_sum is 1
     s_slt <= conv_std_logic_vector(1, g_width) when s_sum(g_width-1) = '1' else
-        conv_std_logic_vector(0, g_width);
+             conv_std_logic_vector(0, g_width);
 
     with i_alucont(1 downto 0) select
-        o_result <= i_a and i_b when "00",
-                    i_a or  i_b when "01",
-                    s_sum     when "10",
-                    s_slt     when others;
+        preout <= i_a and i_b when "00",
+                  i_a or  i_b when "01",
+                  s_sum       when "10",
+                  s_slt       when others;
+
+    o_result  <= preout;
+    o_zerodet <= not or_reduce(preout);
 
 end Behavioral; -- alu
--- mystuff/src/alu_tb.vhd
+++ mystuff/src/alu_tb.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity alu_tb is
     generic(
-        ent_width   : integer   := 32   -- g_width-bit wide registers
+        ent_width : integer := 32 -- g_width-bit wide registers
     );
 end alu_tb;
 
@@ -58,84 +58,91 @@ architecture Behavioral of alu_tb is
 
     component alu is
         generic(
-            g_width: integer
+            g_width : integer
         );
         port(
-            i_a         : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
-            i_b         : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
-            i_alucont   : in  STD_LOGIC_VECTOR(2 downto 0);
-            o_result    : out STD_LOGIC_VECTOR(g_width-1 downto 0)
+            i_a       : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
+            i_b       : in  STD_LOGIC_VECTOR(g_width-1 downto 0);
+            i_alucont : in  STD_LOGIC_VECTOR(2 downto 0);
+            o_zerodet : out STD_LOGIC;
+            o_result  : out STD_LOGIC_VECTOR(g_width-1 downto 0)
         );
     end component; -- alu
 
-    -- signal s_clk        : STD_LOGIC;
-    signal s_a          : STD_LOGIC_VECTOR(ent_width-1 downto 0);
-    signal s_b          : STD_LOGIC_VECTOR(ent_width-1 downto 0);
-    signal s_alucont    : STD_LOGIC_VECTOR(2 downto 0);
-    signal s_result     : STD_LOGIC_VECTOR(ent_width-1 downto 0);
+    signal si_a       : STD_LOGIC_VECTOR(ent_width-1 downto 0);
+    signal si_b       : STD_LOGIC_VECTOR(ent_width-1 downto 0);
+    signal si_alucont : STD_LOGIC_VECTOR(2 downto 0);
+    signal so_zerodet : STD_LOGIC;
+    signal so_result  : STD_LOGIC_VECTOR(ent_width-1 downto 0);
 
 begin
     
     alu_DUT : alu
         generic map(
-            g_width   => ent_width
+            g_width => ent_width
         )
         port map(
-            i_a         => s_a,
-            i_b         => s_b,
-            i_alucont   => s_alucont,
-            o_result    => s_result
+            i_a       => si_a,
+            i_b       => si_b,
+            i_alucont => si_alucont,
+            o_zerodet => so_zerodet,
+            o_result  => so_result
         );
-
-    -- s_clk_gen : process
-    -- begin
-    --     s_clk <= '1';
-    --     wait for 5 ns;
-    --     s_clk <= '0';
-    --     wait for 5 ns;
-    -- end process; -- s_clk_gen
 
     manual_signals : process
     begin
         -- do 1 AND 3
-        s_a         <= x"00000001";
-        s_b         <= x"00000003";
-        s_alucont   <= "000";
+        si_a         <= x"00000001";
+        si_b         <= x"00000003";
+        si_alucont   <= "000";
         wait for 5 ns;
-        assert s_result = x"00000001" report "do 1 AND 3 failed";
+        assert so_result  = x"00000001" report "do 1 AND 3 failed";
+        assert so_zerodet =  '0'        report "do 1 AND 3 failed in zerodet";
         wait for 5 ns;
         -- do 1 OR 3
-        s_a         <= x"00000001";
-        s_b         <= x"00000003";
-        s_alucont   <= "001";
+        si_a         <= x"00000001";
+        si_b         <= x"00000003";
+        si_alucont   <= "001";
         wait for 5 ns;
-        assert s_result = x"00000003" report "do 1 OR 3 failed";
+        assert so_result  = x"00000003" report "do 1 OR 3 failed";
+        assert so_zerodet =  '0'        report "do 1 OR 3 failed in zerodet";
         wait for 5 ns;
         -- do 1 ADD 1
-        s_a         <= x"00000001";
-        s_b         <= x"00000001";
-        s_alucont   <= "010";
+        si_a         <= x"00000001";
+        si_b         <= x"00000001";
+        si_alucont   <= "010";
         wait for 5 ns;
-        assert s_result = x"00000002" report "do 1 ADD 1 failed";
+        assert so_result  = x"00000002" report "do 1 ADD 1 failed";
+        assert so_zerodet =  '0'        report "do 1 ADD 1 failed in zerodet";
         wait for 5 ns;
         -- do 1 SUB 1
-        s_a         <= x"00000001";
-        s_b         <= x"00000001";
-        s_alucont   <= "110";
+        si_a         <= x"00000001";
+        si_b         <= x"00000001";
+        si_alucont   <= "110";
         wait for 5 ns;
-        assert s_result = x"00000000" report "do 1 SUB 1 failed";
+        assert so_result  = x"00000000" report "do 1 SUB 1 failed";
+        assert so_zerodet =  '1'        report "do 1 SUB 1 failed in zerodet";
+        wait for 5 ns;
+        -- do 0 SUB 1
+        si_a         <= x"00000000";
+        si_b         <= x"00000001";
+        si_alucont   <= "110";
+        wait for 5 ns;
+        assert so_result  = x"FFFFFFFF" report "do 1 SUB 1 failed";
+        assert so_zerodet =  '0'        report "do 1 SUB 1 failed in zerodet";
         wait for 5 ns;
         -- do 1 SLT 1
-        s_a         <= x"00000000";
-        s_b         <= x"00000001";
-        s_alucont   <= "111";
+        si_a         <= x"00000000";
+        si_b         <= x"00000001";
+        si_alucont   <= "111";
         wait for 5 ns;
-        assert s_result = x"00000001" report "do 1 SLT 0 failed";
+        assert so_result  = x"00000001" report "do 1 SLT 0 failed";
+        assert so_zerodet =  '0'        report "do 1 SLT 0 failed in zerodet";
         wait for 5 ns;
         -- reset everything to 0
-        s_a         <= x"00000000";
-        s_b         <= x"00000000";
-        s_alucont   <= "000";
+        si_a         <= x"00000000";
+        si_b         <= x"00000000";
+        si_alucont   <= "000";
         wait;
     end process; -- manual_signals
 
